3465ada3685fa66d5cc1ac2566e81f0bc17fb499
Tymoteusz Bleja <tymoteusz.bleja@outlook.com>
2016-12-07 20:02:11 +0100

    Add game background

--- app/src/components/game/Game.js
+++ app/src/components/game/Game.js
@@ -2,6 +2,8 @@ import 'pixi';
 import 'p2';
 import Phaser from 'phaser';
 import React, {Component} from 'react';
+import grass from './grass.png';
+import football from './football.png';
 
 export default class Game extends Component {
   shouldComponentUpdate() {
@@ -9,20 +11,23 @@ export default class Game extends Component {
   }
 
   preload() {
-
+    this.game.load.image('grass', grass);
+    this.game.load.spritesheet('ball', football);
   }
 
   create() {
-
+    this.game.add.tileSprite(0, 0, window.innerWidth, window.innerHeight, 'grass');
   }
 
   update() {
-    
+
   }
 
   componentDidMount() {
     const {preload, create, update} = this;
-    this.game = new Phaser.Game(800, 600, Phaser.AUTO, this.refs.gameContainer, {preload, create, update});
+
+    this.game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO,
+      this.refs.gameContainer, {preload, create, update});
   }
 
   render() {
